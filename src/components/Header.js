3d29c4d647e9d353160c3f24dd24ac80c0c108d0
estradanicolas <estradanicolas@gmail.com>
2020-12-04 16:37:00 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <div className="title">Find your next job in tech!</div>
+  </header>
+);
+
+export default Header;
